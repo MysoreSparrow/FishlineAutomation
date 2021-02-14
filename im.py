0ca0b2958870206239e3748453c3a1b3430fc55d
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-02-14 23:42:02 +0100

    regex error fixed
    regex error plus splitting channels fixed and now loaded with pims

--- im.py
+++ im.py
@@ -1,13 +1,12 @@
 import os
-import sys
 import re
-import numpy as np
 import skimage.io as io
 import glob
-
-#path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22/1/'
+import matplotlib.pyplot as plt
+import numpy as np
+path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22/1/'
 outpath = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/'
-path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/d/'
+#path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/d/'
 #tiffiles = os.listdir(path) # this reads the image name as string type.
 
 #print(tiffiles[0])
@@ -16,7 +15,7 @@ path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/d/'
 #sig_image_list = [tiffiles[0] for tiffiles[0] in tiffiles if not re.search("_ch0{2}", tiffiles[0])]
 
 
-#combined_image_list = \
+ref_counter = sig_counter = 0
 ref_image_list = sig_image_list = combined_image_list = []
 for img in glob.glob(os.path.join(path, "*.tif")):
     #print(img)
@@ -24,39 +23,42 @@ for img in glob.glob(os.path.join(path, "*.tif")):
     image = io.imread(img)# this is reading the actual image itself
     print("##################################")
     print("name of the file", img)
-    #print(image)
-    # Selection/Splitting of channels
-    #ref_image_list = [img for img in os.listdir(path) if re.search("_ch0{2}", img)]
-    #sig_image_list = [img for img in os.listdir(path) if re.search("_ch01{1}", img)]
-#print(ref_image_list)
-#print(sig_image_list)
 
     if re.search("_ch0{2}", str(image_name)):
-        print(f"Adding {image_name} to ref_image_list")
-        ref_image_list.append(image)
-        #r1.append(image_name)
+        ref_counter += 1
+        print(f" ref_counter= {ref_counter}.Adding {image_name} to ref_image_list")
+        #converting to arrays
+        ref_image_array = np.array(image)
+        ref_image_list.append(ref_image_array)
+        #saving the image
+        refpath = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22' \
+                  '/1/ref_image_dir/'
+        if not os.path.exists(refpath):
+            os.makedirs(refpath)
+        for k in range(len(ref_image_list)):
+            plt.imsave(os.path.join(refpath, str(image_name)+".tiff"), ref_image_list[k], cmap='gray')
+
     elif re.search("_ch01{1}", str(image_name)):
-        print(f"Adding {image_name} to sig_image_list")
-        sig_image_list.append(image)
-        #s1.append(image_name)
+        sig_counter += 1
+        print(f"sig_counter= {sig_counter}.Adding {image_name} to sig_image_list")
+        # converting to arrays
+        sig_image_array = np.array(image)
+        sig_image_list.append(sig_image_array)
+        #saving
+        sigpath = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22' \
+                  '/1/sig_image_dir/'
+        if not os.path.exists(sigpath):
+            os.makedirs(sigpath)
+        for j in range(len(sig_image_list)):
+            plt.imsave(os.path.join(sigpath, str(image_name)+".tiff"), sig_image_list[j], cmap='gray')
+
     else:
         print("cant recognize")
 
-#print(len(ref_image_list))
-    #print("In R1", r1)
-print("ref_image_list", ref_image_list)
-print("sig_image_list", sig_image_list)
-#print("In S1", s1)
-
-# why is if condition not working that for each iteration same
-# image is getting added to both ref_image_list and sig_image_list?
-
-    #combined_image_list.append([img, image])
-    #combined_image_list = [[image_name, image]]
-
-    #print(combined_image_list)
-    #print(combined_image_list[0])# this will give the img or names of files
-    #print(combined_image_list[1]) #gives the values of the image
+#TODO: create a folder for saving the respective images into separate folders
+#TODO: save the respective images into the folders
+#TODO: Read the images from each directory using pims
+#TODO: or just load an image sequence with the regex expression for loading the image
 
 
 """
@@ -90,3 +92,15 @@ print(ref_image_list)
 print(len(sig_image_list))
 print(sig_image_list)
 """
+#print(image)
+    # Selection/Splitting of channels
+    #ref_image_list = [img for img in os.listdir(path) if re.search("_ch0{2}", img)]
+    #sig_image_list = [img for img in os.listdir(path) if re.search("_ch01{1}", img)]
+#print(ref_image_list)
+#print(sig_image_list)
+
+# print("ref_image_list", ref_image_list)
+# print("sig_image_list", sig_image_list)
+# print(len(ref_image_list))
+# print(len(sig_image_list))
+# print("ref_image_list", ref_image_list)

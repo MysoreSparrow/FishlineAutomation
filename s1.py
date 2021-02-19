283efe1b31adb97cd29774aa58a24af9d073376a
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-02-19 18:48:28 +0100

    image collection fixed

--- s1.py
+++ s1.py
@@ -5,7 +5,7 @@
 #DONE: Read the images from each directory using pims
 #DONE: or just load an image sequence with the regex expression for loading the image
 #TODO: check voxel depth and set voxel depth to 1
-#TODO: check Image type or PixelIDType and set it to 8 bit integer type
+#DONE: check Image type or PixelIDType and set it to 8 bit integer type
 #TODO: Rotate image
 #TODO: Save after processing as both .nrrd and .tiff in respective folders along with original
 
@@ -14,10 +14,17 @@ import os
 import re
 import shutil
 from pims import ImageSequence
+import numpy as np
+from skimage import io
+import matplotlib.pyplot as plt
+#import SimpleITK as sitk
 
 source_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22/2/'
 dest_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22/2/ref'
 dest_path1 = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1-ImageProcessing/data/201126_bhlhe22/original/201126_bhlhe22/2/sig'
+
+def imread_convert(f):
+    return plt.imread(f).astype(np.uint8)
 
 #Check if each file belongs to Ch00 or Ch01 and split Channels( move files to respective location
 ref_counter = sig_counter = 0
@@ -36,15 +43,21 @@ for file in os.listdir(source_path):
     else:
         print("Its a Folder. Unwanted/Unrecognized file format.")
 
-#Load images sequentially as per respective channels
-ref_image = ImageSequence(dest_path + '/*.tif')
-print(f'The ref_image details: {len(ref_image)} frames with dimensions of {ref_image.frame_shape}')
-sig_image = ImageSequence(dest_path1 + '/*.tif')
-print(f'The sig_image details: {len(sig_image)} frames with dimensions of {sig_image.frame_shape}')
+#Load images sequentially as per respective channels using pims ImageSequence
+#ref_image = ImageSequence(dest_path + '/*.tif')
+#print(f'The ref_image details: {len(ref_image)} frames with dimensions of {ref_image.frame_shape}')
+#sig_image = ImageSequence(dest_path1 + '/*.tif')
+#print(f'The sig_image details: {len(sig_image)} frames with dimensions of {sig_image.frame_shape}')
 
+#Load images sequentially as per respective channels using skimage imageCollection
+#ref_image = io.ImageCollection(dest_path + '/*.tif', load_func=imread_convert)
+ref_image = io.ImageCollection(dest_path + '/*.tif')
+print(f'The ref_image details: {len(ref_image)} frames')
+sig_image = io.ImageCollection(dest_path1 + '/*.tif')
+print(f'The sig_image details: {len(sig_image)} frames')
 
-#for frame in ref_image:
-#    print(frame.shape)
-
-#def check_datatype(image):
-#    if not image.dtype = 'uint8':
+#Convert these image collection object (sequential images) into a single image
+rimage = io.concatenate_images(ref_image)# rimage is a np.array
+print(f"The rimage has dimensions : {rimage.shape}")
+simage = io.concatenate_images(sig_image)
+print(f"The simage has dimensions : {simage.shape}")

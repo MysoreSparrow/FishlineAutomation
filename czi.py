d9ca07645057133524c04a89364a18b62a458eaf
joe <joe@neuro.mpg.de>
2021-03-25 21:34:53 +0100

    Example for the channel iteration +Pycharm autoformat

--- czi.py
+++ czi.py
@@ -7,8 +7,8 @@ from aicspylibczi import CziFile
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-c_path = f'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/'
-saving_path = f'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/test/'
+c_path = r'C:\Users\joe\Downloads\czidata'
+saving_path = r'./export/'
 
 # TODO: Read CZI file from a given folder.
 # TODO: Read metadata for each file and based on channel number split into separate channels.
@@ -17,20 +17,34 @@ saving_path = f'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/test/'
 
 for file in os.listdir(c_path):
     czi = CziFile(os.path.join(c_path, file))
-    l = [md[0] for md in czi.read_subblock_metadata(B=0, S=0, C=0,T=0)]
-    #print(l)
+    print(czi.dims)  # get which dimensions are present
+    print(czi.dims_shape())  # shape of the data over those dims
+    l = [md[0] for md in czi.read_subblock_metadata(B=0, S=0, C=0, T=0)]
+    # print(l)
+
+    for ch_num in range(*czi.dims_shape()[0]['C']):
+        print(ch_num)
+        imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0)
+        # this will give a 3d array when squeezed, ZYX
+
+        # if you wanted to iterate over each Z:
+        # for z_plane in range(*czi.dims_shape()[0]['Z']):
+        #     print(z_plane)
+        #     imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
+        #     print(imgarray.shape)
 
     for index in range(len(l)):
-        #print(l[index]['C'])## Gives the actual value of channel number here.
-        print(l[index])# Gives all of the dict values
+        # for z in _z
+        # print(l[index]['C'])## Gives the actual value of channel number here.
+        print(l[index])  # Gives all of the dict values
         if l[index]['C'] == 0:
-            imgarray, shp = czi.read_image(B=0, S=0, C=0, T=0)
+            imgarray, shp = czi.read_image(B=0, S=0, C=0, T=0, Z=0)
             print('image array shape', imgarray.shape)
-            #(1, 1, 1, 370, 1521, 801)
+            # (1, 1, 1, 370, 1521, 801)
             a1 = np.transpose(np.squeeze(imgarray))
             print('transpose shape:', a1.shape)
-            #(801, 1521, 370)
-            #Q: Why does the third dimension still have 370..isnt this is each slice?
+            # (801, 1521, 370)
+            # Q: Why does the third dimension still have 370..isnt this is each slice?
             a2 = np.stack(a1, axis=0)
             print(a2.shape)
             print('############################')
@@ -44,21 +58,15 @@ for file in os.listdir(c_path):
             # a2 = a1
             # print(a2.shape)
 
-            #tiff.imwrite(os.path.join(c_path, file), a1)
-            #with tiff.TiffWriter(os.path.join(line_path, f"{item}"), imagej=True) as tifw:
+            # tiff.imwrite(os.path.join(c_path, file), a1)
+            # with tiff.TiffWriter(os.path.join(line_path, f"{item}"), imagej=True) as tifw:
             #    tifw.write(rotated_image.astype('uint8'), resolution=(1 / V_x, 1 / V_y),
             #               metadata={'spacing': 1.0, 'unit': 'um', 'axes': 'ZYX'})
 
-
-
-            #for key in l[index]:
+            # for key in l[index]:
             #    print(l[index][key])
-                #B, C, T, Z = l[index][key]
+            # B, C, T, Z = l[index][key]
 
-            #img, shp = czi.read_image(B=0, S=0)
-            #print(img.shape)
-            #print(shp)
-
-
-
-
+            # img, shp = czi.read_image(B=0, S=0)
+            # print(img.shape)
+            # print(shp)

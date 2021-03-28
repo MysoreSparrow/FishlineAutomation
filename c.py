2188d5282aeed8488c66aab3531a6236c50b7faf
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-03-28 20:59:41 +0200

    Add files via upload

--- c.py
+++ c.py
@@ -1,43 +1,49 @@
 import os
-import re
-import nrrd
 import tifffile as tiff
 import numpy as np
 from aicspylibczi import CziFile
-from pathlib import Path
-import matplotlib.pyplot as plt
+import time
 
 c_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/'
 saving_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/test/'
 
-# TODO: Read CZI file from a given folder.
-# TODO: Read metadata for each file and based on channel number split into separate channels.
+# DONE: Read CZI file from a given folder.
+# DONE: Read metadata for each file and based on channel number split into separate channels.
 # TODO: Save each channel as a separate tif/tiff file with correct channel name.
 
-image_list = []
+# def _write_channeltiff(f):
+#     with tiff.TiffFile(f, mode='r+b') as tif:
+#         tiff.imwrite(os.path.join(c_path, f'{name}_ch{ch_num}.tif'), image_list_stack)
+#
+start = time.time()
 for file in os.listdir(c_path):
     print(file)
+    name, ext = file.split('.')
+    #print(name, ext)
     czi = CziFile(os.path.join(c_path, file))
-    print(czi.dims)  # get which dimensions are present
-    print(czi.dims_shape())  # shape of the data over those dims
+    # print(czi.dims)  # get which dimensions are present
+    # print(czi.dims_shape())  # shape of the data over those dims
 
     for ch_num in range(*czi.dims_shape()[0]['C']):
         print('ch_num:',ch_num)
         #imgarray, shp1 = czi.read_image(B=0, S=0, C=ch_num, T=0)
         # this will give a 3d array when squeezed, ZYX
         #print(imgarray.shape)
-
+        image_list = []
         # if you wanted to iterate over each Z:
         for z_plane in range(*czi.dims_shape()[0]['Z']):
             print('z_plane:',z_plane)
             imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
-            print(imgarray.shape)
+            # print(imgarray.shape)
             image = np.squeeze(imgarray)
-            print(image.shape)
+            # print(image.shape)
             image_list.append(image)
-            print(len(image_list))
-        image_list_stack = np.stack(image_list)
-        #tiff.imwrite(os.path.join(c_path, file+f'_ch{ch_num}.tif'), image_list_stack)
-        # with tiff.TiffWriter(os.path.join(c_path, f"{file[:4]}"+f'_ch{ch_num}.tif'), imagej=True) as tifw:
-        #     tifw.write(image_list_stack.astype('uint8'), metadata={'spacing': 1.0, 'unit': 'um', 'axes': 'ZYX'})
+            # print(len(image_list))
+            image_list_stack = np.stack(image_list)
+            #tiff.imwrite(os.path.join(c_path, f'{name}_ch{ch_num}.tif'), image_list_stack)
+            # with tiff.TiffWriter(os.path.join(c_path, f'{name}_ch{ch_num}.tif')) as tifw:
+            #     tifw.write(image_list_stack.astype('uint8'), metadata={'spacing': 1.0, 'unit': 'um', 'axes': 'ZYX'})
 
+#, imagej=True, bigtiff=True
+end = time.time()
+print('total Execution Time:', end - start, 's')

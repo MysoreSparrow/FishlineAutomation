50606a37d8954f379b05bc26627a8cc3421f5ceb
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-03-31 22:45:47 +0200

    crisper version without permission errors

--- czi.py
+++ czi.py
@@ -12,32 +12,6 @@ original_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/original/'
 # DONE: Read metadata for each file and based on channel number split into separate channels.
 # TODO: Save each channel as a separate tif/tiff file with correct channel name without permission errors
 
-
-def czi_splitchannel(f):
-    czi = CziFile(f)
-    #print(czi.dims)  # get which dimensions are present
-    #print(czi.dims_shape())  # shape of the data over those dims
-    # with tiff.TiffFile(czi, mode='w+b') as czi:
-    for ch_num in range(*czi.dims_shape()[0]['C']):
-        print('ch_num:',ch_num)
-        imgarray, shp1 = czi.read_image(B=0, S=0, C=ch_num, T=0)
-        # this will give a 3d array when squeezed, ZYX
-        #print(imgarray.shape)
-        image_list = []
-        # if you wanted to iterate over each Z:
-        for z_plane in range(*czi.dims_shape()[0]['Z']):
-            #print('z_plane:',z_plane)
-            imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
-            # print(imgarray.shape)
-            image = np.squeeze(imgarray)
-            # print(image.shape)
-            image_list.append(image)
-            # print(len(image_list))
-            channel_image_stack = np.stack(image_list)
-            print(channel_image_stack.shape)
-            return channel_image_stack.astype('uint8')
-
-
 if not os.path.exists(original_path):
     print(f'Creating {original_path}')
     os.makedirs(original_path, exist_ok=True)
@@ -48,18 +22,15 @@ for file in os.listdir(c_path):
         print(file)
         name, ext = file.split('.')
         #print(name, ext)
-        #czi_split_image = czi_splitchannel(os.path.join(c_path, file))
 
         czi = CziFile(os.path.join(c_path, file))
-        for ch_num in range(*czi.dims_shape()[0]['C']):
-            print('ch_num:', ch_num)
-            imgarray, shp1 = czi.read_image(B=0, S=0, C=ch_num, T=0)
-            # this will give a 3d array when squeezed, ZYX
-            # print(imgarray.shape)
-            image_list = []
-            # if you wanted to iterate over each Z:
-            for z_plane in range(*czi.dims_shape()[0]['Z']):
-                # print('z_plane:',z_plane)
+        max_channels = range(*czi.dims_shape()[0]['C'])
+        max_slices = range(*czi.dims_shape()[0]['Z'])
+        image_list = []
+        #for ch_num, z_plane in zip(max_channels, max_slices):
+        for ch_num in max_channels:
+            for z_plane in max_slices:
+                print('ch_num:', ch_num, 'z_plane:', z_plane)
                 imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
                 # print(imgarray.shape)
                 image = np.squeeze(imgarray)
@@ -67,14 +38,7 @@ for file in os.listdir(c_path):
                 image_list.append(image)
                 # print(len(image_list))
                 channel_image_stack = np.stack(image_list)
-                print(channel_image_stack.shape)
+                #print(channel_image_stack.shape)
 
-                #tiff.imwrite(os.path.join(original_path, f'{name}_ch{ch_num}.tif'), channel_image_stack)
-                # with tiff.TiffWriter(os.path.join(original_path, f'{name}_ch{ch_num}.tif')) as tifw:
-                #     tifw.write(czi_split_image.astype('uint8'), metadata={'spacing': 1.0, 'unit': 'um', 'axes': 'ZYX'})
                 with tiff.TiffWriter(os.path.join(original_path, f'{name}_ch{ch_num}.tif')) as tifw:
                     tifw.save(channel_image_stack.astype('uint8'), metadata={'spacing': 1.0, 'unit': 'um', 'axes': 'ZYX'})
-
-#, imagej=True, bigtiff=True
-# end = time.time()
-# print('total Execution Time:', end - start, 's')

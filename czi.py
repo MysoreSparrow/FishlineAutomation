93ee1ec05004c6fa5df7fe361555874281bced30
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-04-01 21:05:18 +0200

    error fixed, ref channel fixed

--- czi.py
+++ czi.py
@@ -4,6 +4,7 @@ import numpy as np
 from aicspylibczi import CziFile
 import time
 
+
 c_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/'
 original_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/original/'
 
@@ -16,7 +17,7 @@ if not os.path.exists(original_path):
     print(f'Creating {original_path}')
     os.makedirs(original_path, exist_ok=True)
 
-
+ref_ch_num = int(input('Enter Reference Channel Number:'))
 #start = time.time()
 for file in os.listdir(c_path):
     if file.endswith('.czi'):
@@ -33,13 +34,15 @@ for file in os.listdir(c_path):
             for z_plane in max_slices:
                 print('file:', file, 'ch_num:', ch_num, 'z_plane:', z_plane)
                 imgarray, shp = czi.read_image(B=0, S=0, C=ch_num, T=0, Z=z_plane)
-                # print(imgarray.shape)
                 image = np.squeeze(imgarray)
-                # print(image.shape)
                 image_list.append(image)
-                # print(len(image_list))
                 channel_image_stack = np.stack(image_list)
-                #print(channel_image_stack.shape)
+                # with tiff.TiffWriter(os.path.join(original_path, f'{name}_ch{ch_num}.tif')) as tifw:
+                #     tifw.write(channel_image_stack.astype('uint8'))
 
-                with tiff.TiffWriter(os.path.join(original_path, f'{name}_ch{ch_num}.tif')) as tifw:
-                    tifw.write(channel_image_stack.astype('uint8'))
+                if ref_ch_num == 0 and ch_num == 0:
+                    with tiff.TiffWriter(os.path.join(original_path, f'{name}_ref.tif')) as tifw:
+                        tifw.write(channel_image_stack.astype('uint8'))
+                else:
+                    with tiff.TiffWriter(os.path.join(original_path, f'{name}_sig.tif')) as tifw:
+                        tifw.write(channel_image_stack.astype('uint8'))

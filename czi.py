01e91981e9a53ea80e3072616ff96f751955203e
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-04-01 21:12:36 +0200

    Final script for czi splitting

--- czi.py
+++ czi.py
@@ -1,18 +1,15 @@
+# * Image Processing Script to process fish lines @ Baier Lab *
+# Author: Keshav Prasad Gubbi
+# DONE: Read CZI file from a given folder.
+# DONE: Read metadata for each file and based on channel number split into separate channels.
+# DONE: Save each channel as a separate tif/tiff file with correct channel name without permission errors.
 import os
 import tifffile as tiff
 import numpy as np
 from aicspylibczi import CziFile
 import time
-
-
 c_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/'
 original_path = 'C:/Users/keshavgubbi/Desktop/ATLAS/S1/data/czidata/original/'
-
-
-# DONE: Read CZI file from a given folder.
-# DONE: Read metadata for each file and based on channel number split into separate channels.
-# TODO: Save each channel as a separate tif/tiff file with correct channel name without permission errors
-
 if not os.path.exists(original_path):
     print(f'Creating {original_path}')
     os.makedirs(original_path, exist_ok=True)
@@ -23,7 +20,6 @@ for file in os.listdir(c_path):
     if file.endswith('.czi'):
         print(file)
         name, ext = file.split('.')
-        #print(name, ext)
 
         czi = CziFile(os.path.join(c_path, file))
         max_channels = range(*czi.dims_shape()[0]['C'])
@@ -37,8 +33,6 @@ for file in os.listdir(c_path):
                 image = np.squeeze(imgarray)
                 image_list.append(image)
                 channel_image_stack = np.stack(image_list)
-                # with tiff.TiffWriter(os.path.join(original_path, f'{name}_ch{ch_num}.tif')) as tifw:
-                #     tifw.write(channel_image_stack.astype('uint8'))
 
                 if ref_ch_num == ch_num:
                     with tiff.TiffWriter(os.path.join(original_path, f'{name}_ref.tif')) as tifw:

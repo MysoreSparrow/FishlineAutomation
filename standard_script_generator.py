fdb78f2befbd29448174bd47aef826299d245c1e
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-09-13 20:38:55 +0200

    Add files via upload

--- standard_script_generator.py
+++ standard_script_generator.py
@@ -13,7 +13,7 @@ L8 = ["#SBATCH --mem=100000 \n", "#SBATCH --mail-type=none \n", "#SBATCH --mail-
       "# For pinning threads correctly: \n", "export OMP_PLACES=cores \n", " \n"]
 L9 = ["antsbin=/u/${USER}/ANTs/antsInstallExample/install/bin/ \n"]
 
-output = ["output1=/ptmp/${USER}/avg_templ/live/Registration/${GAL4}/T_${GAL4}_AVG_ \n",
+output = ["output1=/ptmp/${USER}/avg_templ/live/Registration/${GAL4}/T_${GAL4}_ \n",
           "output2=/ptmp/${USER}/avg_templ/live/reformatted/"f"${GAL4}/T_${GAL4}_AVG_{tag}.nrrd \n",
           "output3=/ptmp/${USER}/avg_templ/live/reformatted/"f"${GAL4}/T_${GAL4}_AVG_GFP.nrrd \n"]
 
@@ -21,18 +21,18 @@ input = ["input1=/ptmp/${USER}/avg_templ/live/averages/"f"${GAL4}_AVG_{tag}.nrrd
          "input2=/ptmp/${USER}/avg_templ/live/averages/"f"${GAL4}_AVG_GFP.nrrd \n"]
 
 
-L10 = ["srun $antsbin/antsRegistration -d 3 \ \n", "--float 1 \ \n", "--verbose 1 \ \n",
+L10 = ["srun $antsbin/antsRegistration -d 3 \ \n", "--float 1 \ \n",
       "-o [${output1},${output2}] \ \n", "--interpolation WelchWindowedSinc \ \n", "--use-histogram-matching 0 \ \n",
       "-r [${template1},${input1},1] \ \n", "-t rigid[0.1] \ \n", "-m MI[${template1},${input1},1,32,Regular,0.25] \ \n",
-      "-c [200x200x200x0,1e-8,10] \ \n", "--shrink-factors 12x8x4x2 \n", "--smoothing-sigmas 4x3x2x1 \ \n",
+      "-c [200x200x200x0,1e-8,10] \ \n", "--shrink-factors 12x8x4x2 \n", "--smoothing-sigmas 4x3x2x1vox \ \n",
       "-t Affine[0.1] \ \n", "-m MI[${template1},${input1},1,32,Regular,0.25] \ \n", "-c [200x200x200x0,1e-8,10] \ \n",
-      "--shrink-factors 12x8x4x2  \ \n", "--smoothing-sigmas 4x3x2x1 \ \n", "-t SyN[0.1,6,0] \ \n",
-      "-m CC[${template1},${input1},1,2] \n", "-c [200x200x200x200x10,1e-7,10] \ \n",
+      "--shrink-factors 12x8x4x2  \ \n", "--smoothing-sigmas 4x3x2x1 \ \n", "-t SyN[0.05,6,0.5] \ \n",
+      "-m CC[${template},${input1},1,2] \n", "-c [200x200x200x200x10,1e-7,10] \ \n",
       "--shrink-factors 12x8x4x2x1 \ \n", "--smoothing-sigmas 4x3x2x1x0 \n"]
 
 
 L11 = ["$antsbin/antsApplyTransforms -d 3 \ \n", "-v 0 \ \n", "-- float \ \n", "-n WelchWindowedSinc \ \n",
-       "-i ${input2} \ \n", "-r ${template1} \ \n", "-o ${output3} \ \n", "-t ${output1}1Warp.nii.gz \ \n",
+       "-i ${input2} \ \n", "-r ${template} \ \n", "-o ${output3} \ \n", "-t ${output1}1Warp.nii.gz \ \n",
        "-t ${output1}0GenericAffine.mat \n", " \n", "rm -rf ${input1} ${input2} \n"]
 
 # \n is placed to indicate EOL (End of Line)

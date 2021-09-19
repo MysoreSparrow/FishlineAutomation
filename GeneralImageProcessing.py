a8ca889e62795692d5271651cc20159613e0fc49
Keshava Prasad Gubbi <44500995+keshavprasadgubbi@users.noreply.github.com>
2021-09-19 23:18:21 +0200

    Final working version

--- GeneralImageProcessing.py
+++ GeneralImageProcessing.py
@@ -7,7 +7,7 @@
 
 # DONE: Check which file is it and then pass it onto respective read function that processes it.
 # DONE: Basic functions ---> convert to 8 bit, enhance contrast, save file into respective format.
-# TODO: Need original reformatted files and most likely an alternative way to enhance contrast for these files
+# DONE: Preserve and rewrite the processed nrrd files with rest of the metadata same as teh original file.
 
 
 import os
@@ -15,40 +15,40 @@ import nrrd
 import cv2 as cv
 
 outpath = r'C:/Users/keshavgubbi/Desktop/nifti/reformatted'
-inpath = r'C:/Users/keshavgubbi/Desktop/LinesReg/210719_rspo1_GCaMP6s_new/reformatted/'
+inpath = r'C:/Users/keshavgubbi/Desktop/nifti/reformat_raw'
 
 
 def read_nrrd_file(f):
     nrrd_image, head = nrrd.read(os.path.join(inpath, f), index_order='F')
     width, height, depth = nrrd_image.shape
-    print(f'Working with Image: {file} with dimensions :({width}, {height}) and has {depth} '
+    print(f' The image {file} has dimensions :({width}, {height}) with {depth} '
           f'slices')
-    print(f'Converting the nrrd file into a int8 (8 bit) numpy array!')
+    datatype = head['type']
+    print(f'The Image has dtype: {datatype}. Converting into a int8 (8 bit) image!')
     ce_image = contrast_enhancement(nrrd_image)
     return ce_image.astype('uint8'), head
 
 
-def image_to_nrrd(image):
+def image_to_nrrd(image, Header):
     # Header = {'units': ['m', 'm', 'm'], 'space units': ['microns', 'microns', 'microns']} #'spacings': [voxel_width,
     # voxel_height, 1e-6]}
-    return nrrd.write(os.path.join(outpath, f"{name}.nrrd"), image)
+    return nrrd.write(os.path.join(outpath, f"{name}.nrrd"), image, header=Header)
 
 
 def contrast_enhancement(f):
-    alpha = 3.0  # Contrast control (1.0-3.0) but 5 is required for my purposes here
-    beta = 1  # Brightness control (0-100). Not to be added beyond 5, to not hamper the signal with salt and peper
+    alpha = 3.0  # Contrast control (1.0-3.0) but 3 is required for my purposes here
+    beta = 1  # Brightness control (0-100). Not to be added beyond 5, to not hamper the signal with salt and pepper
     # noise.
-    # contrast_enhanced_image = exposure.adjust_log(f, 50)
     contrast_enhanced_image = cv.convertScaleAbs(f, alpha=alpha, beta=beta)
     return contrast_enhanced_image
 
 
 for file in os.listdir(inpath):
     if file.endswith('.nrrd'):
-        print(file)
+        print(f'Working with Image: {file}')
         # Read the nrrd file and convert it into a 8-bit numpy array after contrast enhancement
         nrrd_image_array, header = read_nrrd_file(file)
+        # print(header)
         name, ext = file.split('.', 1)
-        print(name)
-        processed_image = image_to_nrrd(nrrd_image_array)
+        processed_image = image_to_nrrd(nrrd_image_array, header)
         print(f'####End of processing {file}!##################')
